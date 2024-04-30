7e2a2b0dcbcc0ca55f43301925d41ec3877401b7
Nishant Borkar <107128913+nishan129@users.noreply.github.com>
2024-04-30 12:35:33 +0530

    Update main.py and remove the login function from main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from src.detection.logger import logging 
+
 from src.detection.pipeline.training_pipeline import TrainingPipeline
 from src.detection.constant.training_pipeline import *
-from src.detection.configuration.s3_operations import S3Operation
+
 
 
 if __name__ == '__main__':
